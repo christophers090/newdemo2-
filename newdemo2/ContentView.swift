c315a2efb81c2b8dd476002c9b579079a74e337a
Chris Sheehan <christopher.sheehan.28@gmail.com>
2023-11-03 09:10:09 -0700

    Initial Commit

---
+++ newdemo2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  newdemo2
+//
+//  Created by Chris Sheehan on 11/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ newdemo2/newdemo2App.swift
@@ -0,0 +1,17 @@
+//
+//  newdemo2App.swift
+//  newdemo2
+//
+//  Created by Chris Sheehan on 11/3/23.
+//
+
+import SwiftUI
+
+@main
+struct newdemo2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
